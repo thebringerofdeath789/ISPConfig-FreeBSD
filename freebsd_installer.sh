f824935641f262e6e9c42c2db9d00bf0aeacbcda
thebringerofdeath789 <thebringerofdeath789@gmail.com>
2018-05-14 21:58:12 -0700

    Update freebsd_installer.sh

--- freebsd_installer.sh
+++ freebsd_installer.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 ### PACKAGE INSTALLATION ### 
-pkg install maildrop nano git bash php72 nginx icu wget php72-pear php72-pear-Net_Socket php72-pear-SOAP php72-pear-Net_Vpopmaild php72-pear-Net_POP3 php72-pear-Crypt_CHAP dovecot-pigeonhole php72-pear-MDB2 php72-pear-MDB php72-pear-File_Passwd php72-pear-DB php72-zip php72-session php72-filter php72-simplexml php72-curl php72-bcmath php72-bz2 php72-ftp php72-gd php72-imap php72-json php72-mysqli php72-mbstring php72-pdo php72-pdo_mysql php72-zlib php72-xml php72-sqlite3 php72-soap php72-openssl mysql80-client mysql80-server awstats webalizer jailkit py27-fail2ban py27-certbot py27-certbot-nginx cyrus-sasl-saslauthd amavisd-new amavisd-milter clamav-milter pure-ftpd bind912 postgrey
+pkg install maildrop nano git bash php72 nginx icu wget php72-pear php72-pear-Net_Socket php72-pear-SOAP php72-pear-Net_Vpopmaild php72-pear-Net_POP3 php72-pear-Crypt_CHAP dovecot-pigeonhole php72-pear-MDB2 php72-pear-MDB php72-pear-File_Passwd php72-pear-DB php72-zip php72-session php72-filter php72-simplexml php72-curl php72-bcmath php72-bz2 php72-ftp php72-gd php72-imap php72-json php72-mysqli php72-mbstring php72-pdo php72-pdo_mysql php72-zlib php72-xml php72-sqlite3 php72-soap php72-openssl mysql80-client mysql80-server awstats webalizer jailkit py27-fail2ban py27-certbot py27-certbot-nginx pure-ftpd bind912 postgrey
 
 
-#copy custom ports custom config to db
+# We use the ports because the dovecot, postfix, and spamassassin packages are not build with mysql support
+# These packages are build directly from the ports tree with mysql enabled.
 pkg add ports/php72-pear-Log-1.13.1.txz
 pkg add ports/php72-pear-Net_SMTP-1.8.0.txz
 pkg add ports/php72-pear-Auth-1.6.4.txz
-
 pkg add ports/dovecot2-2.2.31_1.txz
 pkg add ports/postfix-3.3.0_1,1.txz
-#pkg add ports/spamassassin-3.4.1_11.txz
+pkg add ports/spamassassin-3.4.1_11.txz
+pkg install cyrus-sasl-saslauthd amavisd-new amavisd-milter clamav-milter
 
 #build dovecot with SQL support
 #cd /usr/ports/mail/dovecot2
@@ -40,7 +41,9 @@ openssl req -new -x509 -days 1000 -nodes -out "/usr/local/etc/ssl/certs/dovecot.
 
 rndc-confgen -a
 
-openssl dhparam -out /etc/mail/certs/dh.param 4096
+# This takes a long time, so we will hide the output and put it in the background.
+openssl dhparam -out /etc/mail/certs/dh.param 4096 2>&1 >/dev/null &
+
 #### NGINX CONFIGURATION ###
 mkdir /usr/local/etc/nginx/sites-available
 mkdir /usr/local/etc/nginx/sites-enabled
